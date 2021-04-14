e35bad6243b2a8ba52f9f32f24069be2560b2e4d
Arnaud <asallafranque@protonmail.com>
2021-04-14 17:09:23 +0200

    Création page produit + Accueil fini

--- myScript.js
+++ myScript.js
@@ -31,21 +31,19 @@
 
 
 
-let getTeddies = new XMLHttpRequest();
-getTeddies.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.responseText);
-        console.log(response);
-        console.log(response.length);
-        let tedContain = document.getElementById("test");
+// let getTeddies = new XMLHttpRequest();
+// getTeddies.onreadystatechange = function() {
+//     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+//         let response = JSON.parse(this.responseText);
+//         console.log(response);
+//         console.log(response.length);
+//         let tedContain = document.getElementById("test");
 
-        for(i = 0; i < response.length; i++) {
-            tedContain.innerHTML+='<div class="col-lg-4"><a class="" href="#"><p>Plusieurs couleurs disponibles</p><button type="button" class="btn btn-outline-warning">Plus d\'informations</button></a></div>';
-        }
-        for(i = 0; i < response.length; i++) {
-            document.createElement("div")
-        }
-    }
-};
-getTeddies.open("GET", "https://jwdp5.herokuapp.com/api/teddies");
-getTeddies.send();
+//             for(i = 0; i < response.length; i++) {
+//                 tedContain.innerHTML+='<div class="col-lg-4"><a class="" href=produit.html?id="'+response[i]._id+'"><img class="img-fluid" src="'+response[i].imageUrl+'" alt="'+response[i].name+'"><p>'+response[i].name+'</p><button type="button" class="btn btn-outline-warning">Plus d\'informations</button></a></div>';
+//             }
+//     }
+// };
+// getTeddies.open("GET", "https://jwdp5.herokuapp.com/api/teddies");
+// getTeddies.send();
+
